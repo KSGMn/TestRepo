4935f849e38e6b5fb736a982cc91aade0ec4be42
KSGMn <rlatjsrb789@naver.com>
2024-02-06 22:34:10 +0900

    프린트 메세지 변경

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("변경됨");
+        System.out.println("변경됨2");
     }
 }
